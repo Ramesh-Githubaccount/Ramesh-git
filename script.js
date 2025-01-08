75bc13655631da6e35238038cb0bf156a394e778
Ramesh Bhandari <74866980+Ramesh-Githubaccount@users.noreply.github.com>
2025-01-08 20:40:35 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+<script>
+        flag = 0;
+        function controller(x){
+            flag = flag+x;
+            slides(flag);
+
+        }
+        function slides(num){
+            let slide = document.getElementsByClassName("slide");
+            if(num==slide.length){
+                num=0;
+                flag=0;
+            }
+            if(num<0){
+                num=slide.length-1;
+                flag=slide.length-1;
+            }
+            for(let y of slide){
+                y.style.display = "none";
+            }
+            slide[num].style.display = "block";
+        }
+    </script>
